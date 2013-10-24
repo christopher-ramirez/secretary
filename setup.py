a2885f398db67338e8433d3a9f4241faf168d668
Christopher Ramírez <chris.ramirezg@gmail.com>
2013-10-24 11:28:37 -0600

    Update version number in setup.py.

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ class PyTest(TestCommand):
 
 setup(
     name='secretary',
-    version='0.0.1',
+    version='0.0.2',
     url='https://github.com/christopher-ramirez/secretary',
     license='BSD',
     author='Christopher Ramírez',
