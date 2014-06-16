2f0584b136b5c8685840e1a7d60c99117a5a5687
Christopher Ramírez <blindedbythedark@gmail.com>
2014-06-15 20:36:25 -0600

    Adding support for using row and cell as field reference and updating the template accordingly.

--- secretary.py
+++ secretary.py
@@ -49,13 +49,19 @@ FLOW_REFERENCES = {
 
     'table:table-row'    : 'table:table-row',
     'table-row'          : 'table:table-row',
+    'row'                : 'table:table-row',
     'before::table-row'  : 'table:table-row',
     'after::table-row'   : 'table:table-row',
+    'before::row'        : 'table:table-row',
+    'after::row'         : 'table:table-row',
 
     'table:table-cell'   : 'table:table-cell',
     'table-cell'         : 'table:table-cell',
+    'cell'               : 'table:table-cell',
     'before::table-cell' : 'table:table-cell',
     'after::table-cell'  : 'table:table-cell',
+    'before::cell'       : 'table:table-cell',
+    'after::cell'        : 'table:table-cell',
 }
 
 SUPPORTED_FIELD_REFERECES = ['text:p', 'table:table-row', 'table:table-cell']
