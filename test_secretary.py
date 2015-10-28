9a3862547f59a9b4e399e3c0040fd7b0439d856b
bijan ebrahimy <bijanebrahimi@riseup.net>
2015-10-28 23:17:57 +0330

    added tests for footnotes syntax

--- test_secretary.py
+++ test_secretary.py
@@ -104,6 +104,14 @@ class MarkdownFilterTestCase(TestCase):
             found = re.findall(pattern , result)
             assert len(found) == occurances
     
+    def test_footnotes(self):
+        test_samples = {
+            'hello world. [^1]\n\n[^1]: referenced.\n\n': '<text:p text:style-name="Standard">hello world. <text:note text:id="fn-1" text:note-class="footnote"><text:note-citation>1</text:note-citation><text:note-body><text:p>referenced. </text:p></text:note-body></text:note></text:p>',
+            'foo. [^1]\n bar. [^2] \n\n[^1]: referenced by foo.\n[^2]: referenced by bar\n': '<text:p text:style-name="Standard">foo. <text:note text:id="fn-1" text:note-class="footnote"><text:note-citation>1</text:note-citation><text:note-body><text:p>referenced by foo. </text:p></text:note-body></text:note> bar. <text:note text:id="fn-2" text:note-class="footnote"><text:note-citation>2</text:note-citation><text:note-body><text:p>referenced by bar </text:p></text:note-body></text:note> </text:p>',
+            }
+        for test, expected in test_samples.items():
+            result = self.engine.markdown_filter(test)
+            assert expected in result
 
 if __name__ == '__main__':
     main()
