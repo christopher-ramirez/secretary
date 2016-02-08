5fab35a1e38d588efe6db53590ba8464a1c68895
bijan ebrahimy <bijanebrahimi@riseup.net>
2016-02-08 17:22:20 +0330

    added ordereddict to markdown_map

--- secretary/markdown_map.py
+++ secretary/markdown_map.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python
+try:
+    from collections import OrderedDict
+except:
+    # Python 2.6 backward compatibility
+    from ordereddict import OrderedDict
 
-from random import randint
+from .utils import list_id
+
 
 # Transform map used by the markdown filter. transform_map have
 # instructions of how to transform a HTML style tag into a ODT document
@@ -24,7 +30,6 @@ common_styles = {
             }
         }
     },
-
     'strong': {
         'replace_with': 'text:span',
         'style_attributes': {
@@ -39,89 +44,100 @@ common_styles = {
                 'style:font-weight-complex': 'bold'
             }
         }
-    },
-
-    'p': {
-        'replace_with': 'text:p',
-        'style_attributes': {
-            'style-name': 'Standard'
-        }
     }
 }
 
-transform_map = {
-	'a': {
+transform_map = OrderedDict([
+    # Links
+    ('a', {
 		'replace_with': 'text:a',
 		'attributes': {
 			'xlink:type': 'simple',
 			'xlink:href': ''
-		}
-	},
+		}),
 
-    'p': common_styles['p'],
-    'strong': common_styles['strong'],
-    'em': common_styles['italic'],
-    'b': common_styles['strong'],
-    'i': common_styles['italic'],
+    # Paragraphs
+    ('p', {
+        'replace_with': 'text:p',
+        'style_attributes': {
+            'style-name': 'Standard'
+        }),
 
-    # Heading Styles (Use styles defined in the document)
-    'h1': {
+
+    # Texts
+    ('strong', common_styles['strong']),
+    ('b', common_styles['strong']),
+    ('em', common_styles['italic']),
+    ('i', common_styles['italic']),
+
+    # Headings
+    ('h1', {
         'replace_with': 'text:p',
         'style_attributes': {
             'style-name': 'Heading_20_1'
         }
-    },
-
-    'h2': {
+    }),
+    ('h2', {
         'replace_with': 'text:p',
         'style_attributes': {
             'style-name': 'Heading_20_2'
         }
-    },
-
-    'h3': {
+    }),
+    ('h3', {
         'replace_with': 'text:p',
         'style_attributes': {
             'style-name': 'Heading_20_3'
         }
-    },
-
-    'h4': {
+    }),
+    ('h4', {
         'replace_with': 'text:p',
         'style_attributes': {
             'style-name': 'Heading_20_4'
         }
-    },
+    }),
+    ('h5', {
+        'replace_with': 'text:p',
+        'style_attributes': {
+            'style-name': 'Heading_20_5'
+        }
+    }),
+    ('h6', {
+        'replace_with': 'text:p',
+        'style_attributes': {
+            'style-name': 'Heading_20_6'
+        }
+    }),
 
-    'pre': {
+    # Codes
+    ('pre', {
         'replace_with': 'text:p',
         'style_attributes': {
             'style-name': 'Preformatted_20_Text'
         }
-    },
-
-    'code': {
-        'replace_with': 'text:span',
+    }),
+    ('code', {
+        'replace_with': 'text:p',
         'style_attributes': {
             'style-name': 'Preformatted_20_Text'
         }
-    },
+    }),
 
-    'ul': {
+    # Lists
+    ('ul', {
         'replace_with': 'text:list',
         'attributes': {
-            'xml:id': 'list' + str(randint(100000000000000000,900000000000000000))
+            'xml:id': list_id()
         }
-    },
-
-    'ol': {
+    }),
+    ('ol', {
         'replace_with': 'text:list',
         'attributes': {
-            'xml:id': 'list' + str(randint(100000000000000000,900000000000000000))
+            'xml:id': list_id()
         }
-    },
+    }),
 
-    'li': {
+    # Items
+    ('li', {
         'replace_with': 'text:list-item'
-    },
-}
+    })
+])
---
+++ secretary/utils.py
@@ -0,0 +1,5 @@
+from random import randint
+
+
+def list_id():
+    return 'list%d' % (randint(100000000000000000,900000000000000000))
