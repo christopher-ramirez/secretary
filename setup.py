18d2e70ed713a06460ba95b835c4c1c366815295
Christopher Ramírez <chris.ramirezg@gmail.com>
2017-09-07 12:11:16 -0600

    Increase maintenance version number.

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ class PyTest(TestCommand):
 
 setup(
     name='secretary',
-    version='0.2.16',
+    version='0.2.17',
     url='https://github.com/christopher-ramirez/secretary',
     license='MIT',
     author='Christopher Ramírez',
