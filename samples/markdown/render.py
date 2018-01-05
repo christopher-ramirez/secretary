8023eed700c43ed817fa1d4e79294143b0ab471d
Christopher Ramírez <chris.ramirezg@gmail.com>
2018-01-04 19:30:19 -0600

    Update library path on samples scripts

--- samples/markdown/render.py
+++ samples/markdown/render.py
@@ -3,9 +3,9 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath(os.path.join(__file__, '../../../src')))
+sys.path.insert(0, os.path.abspath(os.path.join(__file__, '../../..')))
 
-from __init__ import Renderer
+from secretary import Renderer
 
 
 def main():
