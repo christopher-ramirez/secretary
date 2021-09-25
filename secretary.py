fc846ba03f88a9daf5cb3dac1f6097a9ecc8eaa1
Julien Enselme <jenselme@jujens.eu>
2021-09-25 18:28:33 +0200

    Make secretary compatible with jinja2 3.x
    It is not compatible any more with 2.x.

--- secretary.py
+++ secretary.py
@@ -35,7 +35,9 @@ from mimetypes import guess_type, guess_extension
 from uuid import uuid4
 from xml.dom.minidom import parseString
 from xml.parsers.expat import ExpatError, ErrorString
-from jinja2 import Environment, Undefined, Markup
+from jinja2 import Environment, Undefined
+# markupsafe is a dependency of jinja2.
+from markupsafe import Markup
 
 PY2 = sys.version_info < (3, 0)
 
@@ -148,8 +150,7 @@ class Renderer(object):
 
         self._compile_tags_expressions()
 
-    @jinja2.evalcontextfilter
-    def finalize_value(self, value, *args):
+    def finalize_value(self, value):
         """Escapes variables values."""
         if isinstance(value, Markup):
             return value
